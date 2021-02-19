2ef44544ccff81187d3b0d6cb9a4c424706e5347
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-19 12:06:41 +0530

    Update 005-A Sin(x).c

--- 005-A Sin(x).c
+++ 005-A Sin(x).c
@@ -1,3 +1,14 @@
+/*
+	Institution		: Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	Title of the Degree	: BCA.
+	Year / Semester		: I / II.
+	Academic Year		: 2018-2019 to 2020-2021. 
+	Title of the Paper 	: C Programming Lab.
+	Title of the Program 	: sin(x).
+	Preapared by 		: M Aravindhan, Assistant Professor, Department of Computer Science,
+				Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+*/
+
 #include<stdio.h>
 #include<math.h>
 

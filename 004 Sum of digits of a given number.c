45c97ca0489f99c83dd45a9a4df78d2b3bd0a880
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-19 12:04:32 +0530

    Update 004 Sum of digits of a given number.c

--- 004 Sum of digits of a given number.c
+++ 004 Sum of digits of a given number.c
@@ -1,3 +1,14 @@
+/*
+	Institution		: Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	Title of the Degree	: BCA.
+	Year / Semester		: I / II.
+	Academic Year		: 2018-2019 to 2020-2021. 
+	Title of the Paper 	: C Programming Lab.
+	Title of the Program 	: Sum of digits of a given number.
+	Preapared by 		: M Aravindhan, Assistant Professor, Department of Computer Science,
+				Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	
+*/
 #include<stdio.h>
 
 int main()
@@ -16,8 +27,6 @@ int main()
 	}
 
 	printf("\nSum of  digit is %d.\n",sum);
-	
-	printf("\n");
 
 	return 0;
 }

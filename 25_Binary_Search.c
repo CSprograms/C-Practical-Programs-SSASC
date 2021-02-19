6812d484d844b224b321fbbc17643e79d301f887
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-19 13:13:13 +0530

    Update and rename 25-binary-search.c to 25_Binary_Search.c

--- 25_Binary_Search.c
+++ 25_Binary_Search.c
@@ -1,8 +1,22 @@
+/*
+	Institution		: Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	Title of the Degree	: BCA.
+	Year / Semester		: I / II.
+	Academic Year		: 2018-2019 to 2020-2021. 
+	Title of the Paper 	: C Programming Lab.
+	Title of the Program 	: Binary Search.
+	Preapared by 		: M Aravindhan, Assistant Professor, Department of Computer Science,
+				Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+*/
+
 #include<stdio.h>
+
 int size;
+
 void bubble(int *x)
 {
 	int i,j,temp;
+
 	for(i=0;i<size;i++)
 	{
 		for(j=0;j<size-2;j++)
@@ -16,11 +30,13 @@ void bubble(int *x)
 		}
 	}	
 }
+
 int binary(int *ary,int search,int low,int high)
 {
 	if(low<=high)
 	{
 		int mid=(low+high)/2;
+
 		if(search<ary[mid])
 			return(binary(ary,search,low,mid-1));
 		else if(search>ary[mid])
@@ -30,25 +46,34 @@ int binary(int *ary,int search,int low,int high)
 	else
 		return -1;
 }
+
 int main()
 {
 	int list[20],i,s,loc;
+
 	printf("\nEnter the size of list : ");
 	scanf("%d",&size);
+	
 	for(i=0;i<size;i++)
 	{
 		printf("Enter value for list[%d] : ",i);
 		scanf("%d",&list[i]);
 	}
+	
 	printf("\nEnter the Element for search : ");
 	scanf("%d",&s);
+	
 	bubble(list);
+	
 	printf("\nThe list after Bubble Sort : ");	
+	
 	for(i=0;i<size;i++)
 		printf("\t%d",list[i]);
+	
 	if((loc=binary(list,s,0,size-1))!=-1)
 		printf("\nThe element was found at location %d.",loc);
 	else
 		printf("\nThe element was not found in the list.");
-    	return 0;
+    	
+	return 0;
 }

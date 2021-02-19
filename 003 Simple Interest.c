6a484c4a90fc98f2ad25e3a8e7163f27f0338513
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-19 12:09:42 +0530

    Update 003 Simple Interest.c

--- 003 Simple Interest.c
+++ 003 Simple Interest.c
@@ -7,7 +7,6 @@
 	Title of the Program 	: Simple Interest.
 	Preapared by 		: M Aravindhan, Assistant Professor, Department of Computer Science,
 				Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
-	
 */
 #include<stdio.h>
 

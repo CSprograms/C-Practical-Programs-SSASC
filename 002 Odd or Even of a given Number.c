0b2045e83880c3de980d2f7cf26367266f485de5
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-19 12:01:01 +0530

    Update 002 Odd or Even of a given Number.c

--- 002 Odd or Even of a given Number.c
+++ 002 Odd or Even of a given Number.c
@@ -1,3 +1,15 @@
+/*
+	Institution		: Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	Title of the Degree	: BCA.
+	Year / Semester		: I / II.
+	Academic Year		: 2018-2019 to 2020-2021. 
+	Title of the Paper 	: C Programming Lab.
+	Title of the Program 	: Odd or Even of a given number.
+	Preapared by 		: M Aravindhan, Assistant Professor, Department of Computer Science,
+				Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	
+*/
+
 #include<stdio.h>
 
 int main()
@@ -13,8 +25,6 @@ int main()
 		printf("\nGiven number is ODD.\n");
 	else
 		printf("\nGiven number is EVEN.\n");
-	
-	printf("\n");
 
 	return 0;
 }

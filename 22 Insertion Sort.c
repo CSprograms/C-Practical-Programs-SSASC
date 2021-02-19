f89d6b7143021e82f96a7eefcc9eb17775fdbf4b
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-19 13:01:12 +0530

    Update and rename 22-Insertion-Sort.c to 22 Insertion Sort.c

--- 22 Insertion Sort.c
+++ 22 Insertion Sort.c
@@ -1,18 +1,33 @@
+/*
+	Institution		: Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	Title of the Degree	: BCA.
+	Year / Semester		: I / II.
+	Academic Year		: 2018-2019 to 2020-2021. 
+	Title of the Paper 	: C Programming Lab.
+	Title of the Program 	: Insertion Sort.
+	Preapared by 		: M Aravindhan, Assistant Professor, Department of Computer Science,
+				Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+*/
+
 #include<stdio.h>
+
 int size;
 
 void insertion(int *x)
 {
 	int temp,i,j;
+
 	for(i=1;i<size;i++)
 	{
 		temp=x[i];
 		j=i-1;
+	
 		while(temp<x[j] && j>-1)
 		{
 			x[j+1]=x[j];
 			j--;
 		}
+		
 		x[j+1]=temp;
 	}
 }
@@ -20,18 +35,24 @@ void insertion(int *x)
 int main()
 {
 	int list[20], i;
+	
 	printf("\nEnter the size of list : ");
 	scanf("%d",&size);
+	
 	for(i=0;i<size;i++)
 	{
 		printf("Enter value for list[%d] : ",i);
 		scanf("%d",&list[i]);
 	}
+	
 	insertion(list);
+	
 	printf("\nThe Sorted Elements : ");
+	
 	for(i=0;i<size;i++)
 	{
 		printf("\t%d",list[i]);
 	}
-    	return 0;
+    	
+	return 0;
 }

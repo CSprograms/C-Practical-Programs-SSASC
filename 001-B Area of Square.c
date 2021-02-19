dbec764e615c5311bc0188d0826abcc08e6b3be8
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-19 11:58:56 +0530

    Update 001-B Area of Square.c

--- 001-B Area of Square.c
+++ 001-B Area of Square.c
@@ -1,3 +1,15 @@
+/*
+	Institution		: Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	Title of the Degree	: BCA.
+	Year / Semester		: I / II.
+	Academic Year		: 2018-2019 to 2020-2021. 
+	Title of the Paper 	: C Programming Lab.
+	Title of the Program 	: Area of Square.
+	Preapared by 		: M Aravindhan, Assistant Professor, Department of Computer Science,
+				Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	
+*/
+
 #include<stdio.h>
 
 int main()

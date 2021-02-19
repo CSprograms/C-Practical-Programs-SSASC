55c1ef6265c189ab1d6ddf1eafe5680675816c08
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-19 12:37:31 +0530

    Update and rename 017-mat-add-sub.c to 017 Matrix Addition & Subtraction.c

--- 017 Matrix Addition & Subtraction.c
+++ 017 Matrix Addition & Subtraction.c
@@ -1,4 +1,13 @@
-/* PRACTICAL PROGRAM 13: MATRIX ADDITION & SUBTRACTION */
+/*
+	Institution		: Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	Title of the Degree	: BCA.
+	Year / Semester		: I / II.
+	Academic Year		: 2018-2019 to 2020-2021. 
+	Title of the Paper 	: C Programming Lab.
+	Title of the Program 	: Matrix Addition & Subtraction.
+	Preapared by 		: M Aravindhan, Assistant Professor, Department of Computer Science,
+				Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+*/
 
 #include<stdio.h>
 #include<stdlib.h>

9eb4ec48b09f977a45522e443eee703f159113ef
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-19 13:16:10 +0530

    Update and rename 27-Payroll.c to 27_Payroll.c

--- 27_Payroll.c
+++ 27_Payroll.c
@@ -1,3 +1,14 @@
+/*
+	Institution		: Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	Title of the Degree	: BCA.
+	Year / Semester		: I / II.
+	Academic Year		: 2018-2019 to 2020-2021. 
+	Title of the Paper 	: C Programming Lab.
+	Title of the Program 	: Payroll.
+	Preapared by 		: M Aravindhan, Assistant Professor, Department of Computer Science,
+				Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+*/
+
 #include<stdio.h>
 
 struct Payroll
@@ -15,6 +26,7 @@ int main()
 	if(!fptr)
 	{
 		printf("\nError in opening file!");
+
 		return 1;
 	}
 

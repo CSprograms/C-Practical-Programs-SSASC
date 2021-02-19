b5f6f1395d5ad38226a0478d5437314c633b5bc7
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-19 11:46:27 +0530

    Update and rename 005-sx.c to 005-A Sin(x).c

--- 005-A Sin(x).c
+++ 005-A Sin(x).c
@@ -12,21 +12,26 @@ double fact(double x)
 int main()
 {
 	double x, n, i, series, sign;
+	
 	printf("Enter value for x: ");
     	scanf("%lf",&x);
 	printf("Enter value for n: ");
     	scanf("%lf",&n);
-    	x=x*3.14178/180;	
+    	
+	x=x*3.14178/180;	
 	series=x;
 	sign=-1;
 	i=3;
+	
 	while(i<=n)	
 	{
   		series = series + sign * (pow(x,i)/fact(i));
 		sign = -sign;
 		i= i + 2;
 	}
+	
 	printf("\nsin(x) \t\t:\t%lf",series);		
     	printf("\nmath.sin(x) \t:\t%lf\n",sin(x));	
+	
 	return 0;
 }

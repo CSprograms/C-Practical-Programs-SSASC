2ae354fc419a908ebb0c46b12adae5ee8661435b
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-19 12:09:22 +0530

    Update 003 Compound Interest.c

--- 003 Compound Interest.c
+++ 003 Compound Interest.c
@@ -7,7 +7,6 @@
 	Title of the Program 	: Compound Interest.
 	Preapared by 		: M Aravindhan, Assistant Professor, Department of Computer Science,
 				Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
-	
 */
 #include<stdio.h>
 #include<math.h>

3f99b2f76c5859ea9ee623fbe6deac6075ca3bfa
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-19 13:09:11 +0530

    Update and rename 23-bubble-sort.c to 23 Bubble Sort.c

--- 23 Bubble Sort.c
+++ 23 Bubble Sort.c
@@ -1,4 +1,16 @@
+/*
+	Institution		: Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	Title of the Degree	: BCA.
+	Year / Semester		: I / II.
+	Academic Year		: 2018-2019 to 2020-2021. 
+	Title of the Paper 	: C Programming Lab.
+	Title of the Program 	: Bubble Sort.
+	Preapared by 		: M Aravindhan, Assistant Professor, Department of Computer Science,
+				Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+*/
+
 #include<stdio.h>
+
 int size;
 void bubble(int *x)
 {
@@ -20,18 +32,24 @@ void bubble(int *x)
 int main()
 {
 	int list[20], i;
+	
 	printf("\nEnter the size of list : ");
 	scanf("%d",&size);
+	
 	for(i=0;i<size;i++)
 	{
 		printf("Enter value for list[%d] : ",i);
 		scanf("%d",&list[i]);
 	}
+	
 	bubble(list);
+	
 	printf("\nThe Sorted Elements : ");
+	
 	for(i=0;i<size;i++)
 	{
 		printf("\t%d",list[i]);
 	}
-    	return 0;
+    	
+	return 0;
 }

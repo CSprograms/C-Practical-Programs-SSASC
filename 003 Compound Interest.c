b499f9b47fbf88b06ffd05dc009d5a36367359f8
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-19 12:02:28 +0530

    Update 003 Compound Interest.c

--- 003 Compound Interest.c
+++ 003 Compound Interest.c
@@ -1,3 +1,14 @@
+/*
+	Institution		: Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	Title of the Degree	: BCA.
+	Year / Semester		: I / II.
+	Academic Year		: 2018-2019 to 2020-2021. 
+	Title of the Paper 	: C Programming Lab.
+	Title of the Program 	: Compound Interest.
+	Preapared by 		: M Aravindhan, Assistant Professor, Department of Computer Science,
+				Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	
+*/
 #include<stdio.h>
 #include<math.h>
 int main()
@@ -17,8 +28,6 @@ int main()
 	interest = p * pow( 1 + ( (r / 100.0) /n ) , (n*t) ) - p;
 
 	printf("\nCompund Interest : %.2f.\n",interest);
-	
-	printf("\n");
 
 	return 0;
 }

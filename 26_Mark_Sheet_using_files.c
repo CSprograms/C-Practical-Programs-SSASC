8734dee4bb64275b961dc45b8f404c5871fbc7ae
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-19 13:15:01 +0530

    Update and rename 26-Mark-Sheet.c to 26_Mark_Sheet_using_files.c

--- 26_Mark_Sheet_using_files.c
+++ 26_Mark_Sheet_using_files.c
@@ -1,3 +1,14 @@
+/*
+	Institution		: Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	Title of the Degree	: BCA.
+	Year / Semester		: I / II.
+	Academic Year		: 2018-2019 to 2020-2021. 
+	Title of the Paper 	: C Programming Lab.
+	Title of the Program 	: Mark Sheet using files.
+	Preapared by 		: M Aravindhan, Assistant Professor, Department of Computer Science,
+				Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+*/
+
 #include<stdio.h>
 
 struct MarkSheet
@@ -16,6 +27,7 @@ int main()
 	if(!fptr)
 	{
 		printf("\nError in opening file!");
+
 		return 1;
 	}
 
@@ -24,13 +36,14 @@ int main()
 
 	printf("\nEnter Mark 1 : ");
 	scanf("%d",&MS.m1);
+	
 	printf("\nEnter Mark 2 : ");
 	scanf("%d",&MS.m2);
+	
 	printf("\nEnter Mark 3 : ");
 	scanf("%d",&MS.m3);
 
 	MS.tot = MS.m1 + MS.m2 + MS.m3;
-
 	MS.avg = MS.tot / 3.0;
 
 	fprintf(fptr,"%d %d %d %d %d %f\n", MS.rollNo, MS.m1, MS.m2, MS.m3, MS.tot, MS.avg);

284ffbc6cbc416cb47233c110b4fe749e8d1fa0a
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-19 11:59:55 +0530

    Update 001-C Area of Triangle.c

--- 001-C Area of Triangle.c
+++ 001-C Area of Triangle.c
@@ -1,3 +1,15 @@
+/*
+	Institution		: Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	Title of the Degree	: BCA.
+	Year / Semester		: I / II.
+	Academic Year		: 2018-2019 to 2020-2021. 
+	Title of the Paper 	: C Programming Lab.
+	Title of the Program 	: Area of Triangle.
+	Preapared by 		: M Aravindhan, Assistant Professor, Department of Computer Science,
+				Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	
+*/
+
 #include <stdio.h>
 
 int main( )
@@ -13,8 +25,6 @@ int main( )
 	area = (breadth * height)/2;
 
 	printf("\nArea of Triangle is %.2f.\n", area);
-	
-	printf("\n");
 
 	return 0;
 }

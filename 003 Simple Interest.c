43a79aeb70b4053dada64e80712dbb93f105d1b9
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-19 12:03:22 +0530

    Update 003 Simple Interest.c

--- 003 Simple Interest.c
+++ 003 Simple Interest.c
@@ -1,3 +1,14 @@
+/*
+	Institution		: Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	Title of the Degree	: BCA.
+	Year / Semester		: I / II.
+	Academic Year		: 2018-2019 to 2020-2021. 
+	Title of the Paper 	: C Programming Lab.
+	Title of the Program 	: Simple Interest.
+	Preapared by 		: M Aravindhan, Assistant Professor, Department of Computer Science,
+				Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	
+*/
 #include<stdio.h>
 
 int main()
@@ -17,8 +28,6 @@ int main()
 	interest = p * n * (r/100.0);
 
 	printf("\nSimple Interest : %.2f.\n",interest);
-	
-	printf("\n");
 	
 	return 0;
 }

a91aea55a88548044828927324b412f6c1b628af
CSprograms <78929938+CSprograms@users.noreply.github.com>
2021-02-19 11:58:03 +0530

    Update 001-A Area of Rectangle.c

--- 001-A Area of Rectangle.c
+++ 001-A Area of Rectangle.c
@@ -1,10 +1,12 @@
 /*
-	Title of the Degree	: BCA
-	Year / Semester		: I / II
-	Academic Year		: 2018-2019 to 2020-2021 
-	Title of the Paper 	: C Programming Lab
-	Title of the Program 	: Area of Rectangle
-	Preapared by 		: M Aravindhan
+	Institution		: Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
+	Title of the Degree	: BCA.
+	Year / Semester		: I / II.
+	Academic Year		: 2018-2019 to 2020-2021. 
+	Title of the Paper 	: C Programming Lab.
+	Title of the Program 	: Area of Rectangle.
+	Preapared by 		: M Aravindhan, Assistant Professor, Department of Computer Science,
+				Sri Sankara Arts and Science College (Autonomous), Enathur, Kanchipuram - 631 561.
 	
 */
 #include <stdio.h>
